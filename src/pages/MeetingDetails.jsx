771fc1e9aa5405457c932f364fdbabb9780e8b88
xavireina <xavierreina93@gmail.com>
2022-03-18 20:08:52 +0100

    show image on web and on mobile

--- src/pages/MeetingDetails.jsx
+++ src/pages/MeetingDetails.jsx
@@ -13,25 +13,28 @@ function MeetingDetails() {
 
   const isPastMeeting = new Date(meeting.date) <= new Date();
 
-  return (<>
-    <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
-      <div
-        id="profile"
-        className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0">
-        <div className="p-4 md:p-12 text-center lg:text-left">
-          <h1 className="text-3xl font-bold pt-8 lg:pt-0">{meeting.name}</h1>
-          <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
-          <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
-            {meeting.date}
-          </p>
-          <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
-            {meeting.users.length} users joined
-          </p>
-          <p className="pt-8 text-sm">{meeting.description}</p>
-          <div className="pt-12 pb-8">
-            <div className="flex flex-row">
-              {!isPastMeeting && 
-                <JoinButton
+  return (
+    <>
+      <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
+        <div
+          id="profile"
+          className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
+        >
+          <div className="p-4 md:p-12 text-center lg:text-left">
+            <div className="w-full lg:w-2/5 grid place-items-center p-5 md:hidden">
+              <img className="rounded-none lg:rounded-lg lg:block" src={meeting.image} />
+            </div>
+            <h1 className="text-3xl font-bold pt-8 lg:pt-0">{meeting.name}</h1>
+            <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
+            <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">{meeting.date}</p>
+            <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
+              {meeting.users.length} users joined
+            </p>
+            <p className="pt-8 text-sm">{meeting.description}</p>
+            <div className="pt-12 pb-8">
+              <div className="flex flex-row">
+                {!isPastMeeting && (
+                  <JoinButton
                     id={id}
                     name={meeting.name}
                     description={meeting.description}
@@ -39,16 +42,15 @@ function MeetingDetails() {
                     users={meeting.users}
                     navigate={window.location.pathname}
                   />
-                }
+                )}
+              </div>
             </div>
           </div>
         </div>
+        <div className="w-full lg:w-2/5">
+          <img className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block" src={meeting.image} />
+        </div>
       </div>
-      <div className="w-full lg:w-2/5">
-        <img className="rounded-none lg:rounded-lg shadow-2xl hidden lg:block"
-          src={meeting.image}/>
-      </div>
-    </div>
     </>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -21,6 +21,12 @@ function Profile() {
           className="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-75 mx-6 lg:mx-0"
         >
           <div className="p-4 md:p-12 text-center lg:text-left">
+            <div className="w-full lg:w-2/5 grid place-items-center p-5 md:hidden">
+              <img
+                className="rounded-none lg:rounded-lg lg:block"
+                src={user.image || 'https://cdn-icons-png.flaticon.com/512/149/149071.png'}
+              />
+            </div>
             <h1 className="text-3xl font-bold pt-8 lg:pt-0">{user.name}</h1>
             <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
             <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
